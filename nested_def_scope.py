eeb3bf9deeaf88efcaaa24fc80481899404af098
Muhammad <mohammadkhanmk313@gmail.com>
2026-05-15 16:00:16 +0500

    initial commit

--- nested_def_scope.py
+++ nested_def_scope.py
@@ -3,6 +3,17 @@ def outer():
         x = 10
         return x
 
-    print("Inside outer:", inner())
+    result = inner()
+    print("Inside outer:", result)
 
 outer()
+
+# without storing it in a variable
+# def outer():
+#     def inner():
+#         x = 10
+#         return x
+
+#     print("Inside outer:", inner())
+
+# outer()
