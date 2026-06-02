29e8f452aa7c524742b1cca8fc5926214f32cffd
Muhammad <mohammadkhanmk313@gmail.com>
2026-06-02 16:15:41 +0500

    changes made for utf-8

--- generator_file_fetching.py
+++ generator_file_fetching.py
@@ -7,3 +7,14 @@ file_path = "C:\\Users\\moham\\Desktop\\Ineuron_practice\\simple_document_test.t
 
 for line in read_file(file_path):
     print(line)
+
+# with encoding utf-8
+def read_file(file_path):
+    with open(file_path, encoding="utf-8") as file:
+        for i in file:
+            yield i
+
+file_path = "C:\\Users\\moham\\Desktop\\Ineuron_practice\\simple_document_test.txt"
+
+for line in read_file(file_path):
+    print(line)
