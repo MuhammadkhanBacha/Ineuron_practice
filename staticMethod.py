f6695820afb58e058a785e231b1b99f9d8c81463
Muhammad <mohammadkhanmk313@gmail.com>
2026-06-22 15:12:56 +0500

    initial commit

---
+++ staticMethod.py
@@ -0,0 +1,18 @@
+class Math:
+    @staticmethod
+    def show():
+        return "Hi Muhammad"
+
+print(Math.show())
+
+# BOTH ARE THE SAME
+# In first you reutrn a print and in the second you return a string.
+# Second will give you a None at the end if print Math.show().
+
+
+# class Math:
+#     @staticmethod
+#     def show():
+#         print("Hi Muhammad")
+
+# Math.show()
